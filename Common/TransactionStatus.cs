0d2c1df86d4e748a5dde24cb1dc09780bba45b2b
Hieu Ba <phambah08@gmail.com>
2020-02-26 01:13:41 +0700

    entity add

---
+++ Common/TransactionStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop.Common
+{
+    public enum TransactionStatus
+    {
+        Success,
+        Failed
+    }
+}
