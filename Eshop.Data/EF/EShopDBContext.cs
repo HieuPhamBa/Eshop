09bb79d81949071b3b8365d46fcbbb85c4e7b286
Hieu Ba <phambah08@gmail.com>
2020-02-25 07:45:33 +0700

    Entity configure

---
+++ Eshop.Data/EF/EShopDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop.Data.EF
+{
+    public class EShopDBContext: DbContext
+    {
+        public EShopDBContext(DbContextOptions options): base(options)
+        {
+        }
+
+        
+    }
+}
